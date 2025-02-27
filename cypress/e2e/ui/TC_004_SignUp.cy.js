b8d19fc36dcaa6e174ba20c27c206c5e2b8eb039
NGUYỄN SỸ HẢI THANH <thanhproo1412@gmail.com>
2025-02-27 13:43:54 +0700

    add ignore file

--- cypress/e2e/ui/TC_004_SignUp.cy.js
+++ cypress/e2e/ui/TC_004_SignUp.cy.js
@@ -33,7 +33,9 @@ describe('Sign Up Test', () => {
         // Perform sign-up action
         SignUpAction.signUp(testData.name, testData.email, testData.password);
 
+
         // Verify successful sign-up
+        cy.wait(5000); // Wait for 5 seconds
         cy.url().should('include', '/welcome');
 
         cy.wait(5000); // Wait for 5 seconds
